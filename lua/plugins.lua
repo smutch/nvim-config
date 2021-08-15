0dcfe2c68cbfd0822ef44e0dce88fda05d498463
Simon Mutch <smutch@unimelb.edu.au>
2021-08-16 09:52:46 +1000

    Convert fzf options to lua (lots of work!)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,10 +36,10 @@ return require('packer').startup(function(use)
             vim.api.nvim_set_keymap('i', '<C-Space>', 'compe#complete()', {expr = true, noremap = true, silent = true})
 
             -- Set completeopt to have a better completion experience
-            vim.go.completeopt = {'menuone', 'noinsert', 'noselect'}
+            vim.go.completeopt = 'menuone,noinsert,noselect'
 
             -- Avoid showing message extra message when using completion
-            vim.go.shortmess:append({ c = true })
+            vim.opt.shortmess:append({ c = true })
         end
     }
     use 'ray-x/lsp_signature.nvim'
@@ -56,8 +56,8 @@ return require('packer').startup(function(use)
     use {
         'junegunn/vim-easy-align',
         config = function()
-            vim.api.nvim_set_keymap('v', '<Enter>', '<Plug>(EasyAlign)')
-            vim.api.nvim_set_keymap('n', 'gA', '<Plug>(EasyAlign)')
+            vim.api.nvim_set_keymap('v', '<Enter>', '<Plug>(EasyAlign)', {})
+            vim.api.nvim_set_keymap('n', 'gA', '<Plug>(EasyAlign)', {})
         end
     }
     use {
@@ -103,9 +103,111 @@ return require('packer').startup(function(use)
     }
     use 'justinmk/vim-dirvish'
     use {
-        'junegunn/fzf',
+        'junegunn/fzf.vim',
         run = function() vim.fn['fzf#install']() end,
-        requires='junegunn/fzf.vim'
+        requires = 'junegunn/fzf',
+        config = function()
+            -- Advanced customization using autoload functions
+            vim.api.nvim_set_keymap('i', '<C-x><C-k>', 'fzf#vim#complete#word({"left": "15%"})', {noremap = true, expr = true})
+            -- This is the default extra key bindings
+            vim.g.fzf_action = {
+                ['ctrl-t'] = 'tab split',
+                ['ctrl-o'] = 'split',
+                ['ctrl-v'] = 'vsplit'
+            }
+
+            -- Default fzf layout
+            vim.g.fzf_layout = { up = '~40%' }
+
+            -- Customize fzf colors to match your color scheme
+            vim.g.fzf_colors = {
+                fg =      {'fg', 'Normal'},
+                bg =      {'bg', 'Normal'},
+                hl =      {'fg', 'Comment'},
+                ['fg+'] =     {'fg', 'CursorLine', 'CursorColumn', 'Normal'},
+                ['bg+'] =     {'bg', 'CursorLine', 'CursorColumn'},
+                ['hl+'] =     {'fg', 'Statement'},
+                info =    {'fg', 'PreProc'},
+                border =  {'fg', 'Ignore'},
+                prompt =  {'fg', 'Conditional'},
+                pointer = {'fg', 'Exception'},
+                marker =  {'fg', 'Keyword'},
+                spinner = {'fg', 'Label'},
+                header =  {'fg', 'Comment'}
+            }
+
+            -- For Commits and BCommits to customize the options used by 'git log':
+            vim.g.fzf_commits_log_options = '--graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr"'
+
+            -- Mappings and commands
+            vim.api.nvim_set_keymap('n', '<leader>fm', '<plug>(fzf-maps-n)', {})
+            vim.api.nvim_set_keymap('x', '<leader>fm', '<plug>(fzf-maps-x)', {})
+            vim.api.nvim_set_keymap('o', '<leader>fm', '<plug>(fzf-maps-o)', {})
+
+            -- local function t(str)
+                -- return vim.api.nvim_replace_termcodes(str, true, true, true)
+            -- end
+            -- vim.api.nvim_exec([[
+            -- command! -bang -nargs=* Rg
+            -- call fzf#vim#grep(
+              -- 'rg --column --line-number --no-heading --color=always '.shellescape(<q-args>), 1,
+              -- <bang>0 ? fzf#vim#with_preview('up:60%')
+                      -- : fzf#vim#with_preview('right:50%:hidden', '?'),
+              -- <bang>0)
+            -- ]], false)
+
+            vim.api.nvim_set_keymap('n', '<leader>fb',':Buffers<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>ff',':Files %:p:h<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fhf',':History<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fh:',' :History:<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fh/',':History/<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>f:',':Commands<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fw',':Windows<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fs',':Snippets<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>f?',':Helptags<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fg',':GitFiles?<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fl',':Lines<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fL',':BLines<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>ft',':Tags<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>fT',':BTags<CR>', { noremap = true })
+            vim.api.nvim_set_keymap('n', '<leader>f/',':Rg<CR>', { noremap = true })
+
+            vim.api.nvim_set_keymap('i', '<c-x><c-k>','<plug>(fzf-complete-word)', {})
+            vim.api.nvim_set_keymap('i', '<c-x><c-f>','<plug>(fzf-complete-path)', {})
+            vim.api.nvim_set_keymap('i', '<c-x><c-j>','<plug>(fzf-complete-file-ag)', {})
+            vim.api.nvim_set_keymap('i', '<c-x><c-l>','<plug>(fzf-complete-line)', {})
+
+            -- project files
+            function _G.find_git_root()
+                return vim.cmd('system("git rev-parse --show-toplevel 2> /dev/null")[:-2]')
+            end
+            vim.cmd('command! -bang FZFProjectFiles call fzf#vim#files(v:lua.find_git_root(), <bang>0)')
+            vim.api.nvim_set_keymap('n', '<leader>fp', ':FZFProjectFiles<CR>', {noremap=true})
+
+            -- floating fzf
+            -- taken from https://github.com/junegunn/fzf.vim/issues/664#issuecomment-564267298
+            vim.env.FZF_DEFAULT_OPTS = vim.env.FZF_DEFAULT_OPTS .. ' --layout=reverse'
+
+            function _G.FloatingFZF()
+                local height = vim.go.lines
+                local width = vim.go.columns - (vim.go.columns * 2 / 10)
+                local col = (vim.go.columns - width) / 2
+                local col_offset = vim.go.columns / 10
+                local opts = {
+                    relative = 'editor',
+                    row = 1,
+                    col = math.floor(col + col_offset),
+                    width = math.floor(width * 2 / 1),
+                    height = math.floor(height / 2),
+                    style = 'minimal'
+                }
+                local buf = vim.api.nvim_create_buf(false, true)
+                local win = vim.api.nvim_open_win(buf, true, opts)
+                vim.api.nvim_win_set_var(win, 'winhl', 'NormalFloat:TabLine')
+                end
+
+                vim.g.fzf_layout = { window = 'call v:lua.FloatingFZF()' }
+        end
     }
     use 'nvim-lua/plenary.nvim'
     use 'folke/todo-comments.nvim'
