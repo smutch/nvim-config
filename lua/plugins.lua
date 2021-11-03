0dd4759eabfde62d785f3790b79f0bc3c96f6cd5
Simon Mutch <smutch@unimelb.edu.au>
2021-11-03 19:28:53 +1100

    Fix copilot setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -121,7 +121,7 @@ return require('packer').startup(function(use)
     use {
         'github/copilot.vim',
         opt = true,
-        config = function()
+        setup = function()
             vim.api.nvim_set_keymap('i', [[<C-j>]], [[copilot#Accept("<CR>")]], { silent = true, script = true, expr = true })
             vim.g.copilot_no_tab_map = true
         end
