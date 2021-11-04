18c087a27ced5982f2988574d0822e38f75f0aec
Simon Mutch <smutch@unimelb.edu.au>
2021-11-04 01:13:13 +0000

    Start playing with debouncing text changes with LSP
    This is for performance on AWS instances

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -113,7 +113,10 @@ local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
     local opts = {
         on_attach = on_attach,
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        flags = {
+            debounce_text_changes = 150,
+        }
     }
 
     -- Customize the options passed to the server
