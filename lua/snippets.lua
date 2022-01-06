fb6e67b1b4a8e395263a7f43453e88e6e1885dd1
Simon Mutch <smutch@unimelb.edu.au>
2022-01-06 01:43:42 +0000

    Add python ifmain snippet

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -50,5 +50,9 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     draw.to_frame(app, &frame).unwrap();
 }
 ]])))
+    },
+    python = {
+        s({ trig = "ifmain", name = "if __main__", descr = "If this is being run directly" },
+          t({ 'if __name__ == "__main__":', "\t" }), i(0))
     }
 }
