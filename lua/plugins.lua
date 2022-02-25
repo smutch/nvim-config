5c8a2c5a7671e2d8ded359db893d5d47efe9548f
Simon Mutch <smutch@unimelb.edu.au>
2022-02-25 11:40:34 +1100

    Improve yank paste comment commands (gpc)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,6 @@
+            vim.api.nvim_set_keymap('v', 'gdc', '<cmd>call v:lua.___gdc("line")<cr>', {noremap = true})
+            vim.api.nvim_set_keymap('v', 'gdc', '<cmd>call v:lua.___gdc("V")<cr>', {noremap = true})
+            vim.api.nvim_set_keymap('v', 'gpc', '<cmd>call v:lua.___gpc("V")<cr>', {noremap = true})
 -- begin by ensure packer is actually installed!
 -- local execute = vim.api.nvim_command
 local fn = vim.fn
@@ -198,11 +201,33 @@ return require'packer'.startup(function(use)
         config = function()
             require('Comment').setup { mappings = { extended = true }, ignore = { '^$' } }
             vim.api.nvim_set_keymap('n', '<leader><leader>', 'gcc', {})
-            vim.api.nvim_set_keymap('n', 'gcp', 'yygccp', {})
-            vim.api.nvim_set_keymap('n', 'gcP', 'yygccP', {})
             vim.api.nvim_set_keymap('v', '<leader><leader>', 'gc', {})
-            vim.api.nvim_set_keymap('v', 'gp', 'ypgvgc', {})
-            vim.api.nvim_set_keymap('v', 'gP', 'ygvgc`<P', {})
+
+            local U = require("Comment.utils")
+            local A = require("Comment.api")
+
+            function _G.___gpc(vmode)
+                local range = U.get_region(vmode)
+                -- print(vim.inspect(range))
+                local lines = U.get_lines(range)
+                -- print(vim.inspect(lines))
+
+                -- Copying the block
+                local srow = range.erow
+                vim.api.nvim_buf_set_lines(0, srow, srow, false, lines)
+
+                -- Doing the comment
+                A.comment_linewise_op(vmode)
+
+                -- Move the cursor
+                local erow = srow + 1
+                local line = U.get_lines({ srow = srow, erow = erow })
+                local _, col = U.grab_indent(line[1])
+                vim.api.nvim_win_set_cursor(0, { erow, col })
+            end
+
+            vim.api.nvim_set_keymap('v', 'gpc', '<esc><cmd>call v:lua.___gpc("V")<cr>', {noremap = true})
+            vim.api.nvim_set_keymap('n', 'gpc', '<esc><cmd>call v:lua.___gpc()<cr>', {noremap = true})
         end
     }
     use {
