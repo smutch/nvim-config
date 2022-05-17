abbf98d4f42707c52d2f51605c030ebddeb738a7
Simon Mutch <smutch@unimelb.edu.au>
2022-05-17 21:01:45 +1000

    Update telescope config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -395,6 +395,60 @@ return require'packer'.startup(function(use, use_rocks)
                 config = function()
                     require('neoclip').setup()
                     vim.api.nvim_set_keymap('n', '<leader>v', '<cmd>Telescope neoclip<cr>', { noremap = true })
+
+                    require("telescope").setup {
+                        defaults = {
+                            vimgrep_arguments = {
+                                "rg",
+                                "--color=never",
+                                "--no-heading",
+                                "--with-filename",
+                                "--line-number",
+                                "--column",
+                                "--smart-case",
+                            },
+                            prompt_prefix = "   ",
+                            selection_caret = "  ",
+                            entry_prefix = "  ",
+                            initial_mode = "insert",
+                            selection_strategy = "reset",
+                            sorting_strategy = "ascending",
+                            layout_strategy = "horizontal",
+                            layout_config = {
+                                horizontal = {
+                                    prompt_position = "top",
+                                    preview_width = 0.55,
+                                    results_width = 0.8,
+                                },
+                                vertical = {
+                                    mirror = false,
+                                },
+                                width = 0.87,
+                                height = 0.80,
+                                preview_cutoff = 120,
+                            },
+                            -- file_sorter = require("telescope.sorters").get_fuzzy_file,
+                            file_ignore_patterns = { "node_modules" },
+                            -- generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+                            path_display = { "truncate" },
+                            winblend = 0,
+                            border = {},
+                            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                            color_devicons = true,
+                            use_less = true,
+                            set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+                            -- file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+                            -- grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+                            -- qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+                            -- Developer configurations: Not meant for general override
+                            -- buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+                            mappings = {
+                                n = { ["q"] = require("telescope.actions").close },
+                            },
+                        },
+
+                        extensions_list = { "themes", "terms" },
+                    }
                 end
             }, 'nvim-telescope/telescope-symbols.nvim'
         },
