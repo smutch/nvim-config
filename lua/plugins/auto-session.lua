6301a7b804c4663c13916ed3854c912579e943e0
Simon Mutch <smutch@unimelb.edu.au>
2022-06-02 19:55:00 +1000

    Close neotree prior to storing session with auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,10 +1,15 @@
 local M = {}
 
+M.neotree_was_open = false
+
 function M.config()
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+
     require('auto-session').setup {
+        pre_save_cmds = { "tabdo Neotree close" },
+        post_save_cmds = { "tabdo Neotree show" },
         post_restore_cmds = { "stopinsert" }
     }
 end
 
 return M
-
