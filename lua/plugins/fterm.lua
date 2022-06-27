1b04515a8de16f013115f76f65a39818f28ca467
Simon Mutch <smutch@unimelb.edu.au>
2022-06-27 14:09:45 +1000

    Try remapping floating terminal to M-\

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 function M.config()
-    vim.keymap.set('n', '<tab>', '<CMD>lua require("FTerm").toggle()<CR>', {noremap = true})
+    vim.keymap.set('n', [[<M-\>]], '<CMD>lua require("FTerm").toggle()<CR>', {noremap = true})
+    vim.keymap.set('t', [[<M-\>]], '<CMD>lua require("FTerm").toggle()<CR>', {noremap = true})
     vim.keymap.set('n', '<leader>gl', '<CMD>lua require("FTerm").scratch({ cmd = "lazygit" })<CR>')
 end
 
