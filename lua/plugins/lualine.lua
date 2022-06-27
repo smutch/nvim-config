3e57a700b9b8342a2945c7d3f89c854c56f5684a
Simon Mutch <smutch@unimelb.edu.au>
2022-06-27 14:10:08 +1000

    Don't list null-ls as an active server in statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -193,7 +193,7 @@ function M.statusline()
             end
             for _, client in ipairs(clients) do
                 local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                if filetypes and client.name ~= "null-ls" and vim.fn.index(filetypes, buf_ft) ~= -1 then
                     return client.name
                 end
             end
