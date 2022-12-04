65aa35adeec45ff05e7c421b917d62434232ff22
Simon Mutch <smutch@unimelb.edu.au>
2022-12-05 09:30:35 +1100

    Update dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,17 +3,21 @@ local M = {}
 
 function M.config()
     local dap = require 'dap'
-    local Path = require'plenary.path'
+    local Path = require 'plenary.path'
 
-    vim.keymap.set("n", "<leader>dc", require'dap'.continue, {silent=true, noremap=true})
-    vim.keymap.set("n", "<leader>ds", require'dap'.step_over, {silent=true, noremap=true})
-    vim.keymap.set("n", "<leader>di", require'dap'.step_into, {silent=true, noremap=true})
-    vim.keymap.set("n", "<leader>do", require'dap'.step_out, {silent=true, noremap=true})
-    vim.keymap.set("n", "<Leader>db", require'dap'.toggle_breakpoint, {silent=true, noremap=true})
-    vim.keymap.set("n", "<Leader>dB", function() require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, {silent=true, noremap=true})
-    vim.keymap.set("n", "<Leader>dl", function() require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, {silent=true, noremap=true})
-    vim.keymap.set("n", "<Leader>dr", require'dap'.repl.open, {silent=true, noremap=true})
-    vim.keymap.set("n", "<Leader>dp", require'dap'.run_last, {silent=true, noremap=true})
+    vim.keymap.set("n", "<leader>dc", require'dap'.continue, { silent = true, noremap = true })
+    vim.keymap.set("n", "<leader>ds", require'dap'.step_over, { silent = true, noremap = true })
+    vim.keymap.set("n", "<leader>di", require'dap'.step_into, { silent = true, noremap = true })
+    vim.keymap.set("n", "<leader>do", require'dap'.step_out, { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>db", require'dap'.toggle_breakpoint, { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>dB",
+                   function() require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+                   { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>dl",
+                   function() require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end,
+                   { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>dr", require'dap'.repl.open, { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>dp", require'dap'.run_last, { silent = true, noremap = true })
 
     local api = vim.api
     local keymap_restore = {}
@@ -27,19 +31,12 @@ function M.config()
                 end
             end
         end
-        api.nvim_set_keymap(
-        'n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
+        api.nvim_set_keymap('n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
     end
 
     dap.listeners.after['event_terminated']['me'] = function()
         for _, keymap in pairs(keymap_restore) do
-            api.nvim_buf_set_keymap(
-            keymap.buffer,
-            keymap.mode,
-            keymap.lhs,
-            keymap.rhs,
-            { silent = keymap.silent == 1 }
-            )
+            api.nvim_buf_set_keymap(keymap.buffer, keymap.mode, keymap.lhs, keymap.rhs, { silent = keymap.silent == 1 })
         end
         keymap_restore = {}
     end
@@ -70,19 +67,53 @@ function M.config()
             stopOnEntry = false,
             program = function()
                 local program
-                vim.ui.input({ prompt = 'executable: ' }, function(input) program = Path:new(input):absolute() end)
+                vim.ui.input({ prompt = 'executable: ' }, function(input)
+                    program = Path:new(input):absolute()
+                end)
                 return program
             end,
             args = function()
                 local args
                 vim.ui.input({ prompt = 'arguments: ' }, function(input) args = vim.split(input, " ") end)
                 return args
-            end,
+            end
             -- args = function()
             --     return vim.ui.input({ prompt = 'args: ' }, function(input) return vim.split(input, " ") end)
             -- end
         }
     }
+
+    dap.adapters.python = {
+        type = 'executable',
+        command = vim.fn.stdpath('data') .. '/mason/packages/debugpy/venv/bin/python',
+        args = { '-m', 'debugpy.adapter' }
+    }
+
+    dap.configurations.python = {
+        {
+            -- The first three options are required by nvim-dap
+            type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+            request = 'launch',
+            name = "Launch file",
+
+            -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+            program = "${file}", -- This configuration will launch the current file if used.
+            pythonPath = function()
+                local cwd = vim.fn.getcwd()
+                if vim.env.CONDA_PREFIX ~= nil then
+                    return vim.env.CONDA_PREFIX .. '/bin/python'
+                elseif vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                    return cwd .. '/venv/bin/python'
+                elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                    return cwd .. '/.venv/bin/python'
+                else
+                    return '/usr/bin/python'
+                end
+            end
+        }
+    }
+
 end
 
 return M
