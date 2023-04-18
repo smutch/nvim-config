d8d2c7b5ce49156e52dabefcbbea6c0021051174
Simon Mutch <smutch@unimelb.edu.au>
2023-04-18 10:33:11 +1000

    Add smart-splits plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -250,4 +250,26 @@ return {
             }
         end
     },
+    {
+        'mrjones2014/smart-splits.nvim',
+        config = function()
+            require('smart-splits').setup()
+            vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+            vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+            vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+            vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+            -- moving between splits
+            vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+            vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+            vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+            vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+            -- swapping buffers between windows
+            vim.keymap.set('n', '<leader>bh', require('smart-splits').swap_buf_left)
+            vim.keymap.set('n', '<leader>bj', require('smart-splits').swap_buf_down)
+            vim.keymap.set('n', '<leader>bk', require('smart-splits').swap_buf_up)
+            vim.keymap.set('n', '<leader>bl', require('smart-splits').swap_buf_right)
+            -- smart mode
+            vim.keymap.set('n', '<leader>R', require('smart-splits').start_resize_mode)
+        end
+    }
 }
