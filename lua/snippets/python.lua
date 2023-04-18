0ade46492efe7ad13494f834730a170db5ac3bc5
Simon Mutch <smutch@unimelb.edu.au>
2023-04-18 10:17:30 +1000

    Add python cli script snippet

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -1,5 +1,31 @@
 return {
     s({ trig = "ifmain", name = "if __main__", descr = "If this is being run directly" },
-        t({ 'if __name__ == "__main__":', "\t" }), i(0)
-    )
+        t({ 'if __name__ == "__main__":', "    " }), i(0)
+    ),
+
+    s(
+        {
+            trig = "script",
+            name = "script template",
+            descr = "A scaffold for a script"
+        },
+        {
+            t({
+                'from pathlib import Path',
+                '',
+                'import typer',
+                '',
+                '',
+                'def main():',
+                "    "
+            }), i(0),
+            t({
+                '',
+                '',
+                '',
+                'if __name__ == "__main__":',
+                "    typer.run(main)"
+            })
+        }
+    ),
 }
