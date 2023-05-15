8b9dfc406ffdd76110477ca565f4ad789ceb67e4
Simon Mutch <smutch@unimelb.edu.au>
2023-05-15 12:32:59 +1000

    Try to improve cmp ordering

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -28,6 +28,14 @@ return {
             })
         end
     },
+    -- {
+    --     'huggingface/hfcc.nvim',
+    --     opts = {
+    --         api_token = require"system".hf_api_token,
+    --         model = "bigcode/starcoder", -- can be a model ID or an http endpoint
+    --     }
+    -- },
+
     { "zbirenbaum/copilot-cmp", config = true },
 
     {
@@ -43,20 +51,19 @@ return {
             local cmp = require 'cmp'
 
             local has_words_before = function()
+                if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
                 local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") ==
-                nil
+                return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
             end
 
             local default_sources = {
-                { name = 'calc', group_index = 1  },
                 { name = 'path', group_index = 1  },
                 { name = "jupyter", group_index = 1 },
                 { name = 'nvim_lsp', group_index = 1  },
                 { name = 'luasnip', group_index = 1  },
                 { name = 'treesitter', group_index = 1  },
                 { name = 'copilot', group_index = 1  },
+                { name = 'calc', group_index = 2  },
                 { name = 'emoji', group_index = 2  },
                 { name = 'buffer', keyword_length = 5, group_index = 2 },
             }
@@ -97,13 +104,31 @@ return {
                     end, { "i", "s" })
                 },
                 sources = default_sources,
-                formatting = { format = require 'lspkind'.cmp_format({ with_text = true, maxwidth = 50 }) },
+                formatting = {
+                    format = require 'lspkind'.cmp_format(
+                        {
+                            mode = 'symbol_text',
+                            maxwidth = 50,
+                            symbol_map = { Copilot = "" }
+                        }
+                    )
+                },
                 experimental = { native_menu = false, ghost_text = true },
                 sorting = {
+                    priority_weight = 2,
                     comparators = {
-                        cmp.config.compare.offset, cmp.config.compare.exact, cmp.config.compare.score,
-                        require "cmp-under-comparator".under, cmp.config.compare.kind,
-                        cmp.config.compare.sort_text, cmp.config.compare.length, cmp.config.compare.order
+                        cmp.config.compare.offset,
+                        -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+                        cmp.config.compare.exact,
+                        require("copilot_cmp.comparators").prioritize,
+                        cmp.config.compare.score,
+                        cmp.config.compare.recently_used,
+                        cmp.config.compare.locality,
+                        require "cmp-under-comparator".under,
+                        cmp.config.compare.kind,
+                        cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
                     }
                 }
             })
