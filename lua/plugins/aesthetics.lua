28379dcda8e5d3ab09aebcece3ac804c5d3ad249
Simon Mutch <smutch@unimelb.edu.au>
2023-06-15 21:21:07 +1000

    Add edgy plugin

--- lua/plugins/aesthetics.lua
+++ lua/plugins/aesthetics.lua
@@ -106,5 +106,70 @@ return {
             })
         end
     },
-    { 'nvim-tree/nvim-web-devicons' }
+    { 'nvim-tree/nvim-web-devicons' },
+    {
+      "folke/edgy.nvim",
+      event = "VeryLazy",
+      init = function()
+        vim.opt.laststatus = 3
+        vim.opt.splitkeep = "screen"
+      end,
+      opts = {
+        bottom = {
+          {
+            ft = "",
+            size = { height = 0.33 },
+            filter = function(buf, win)
+              return vim.bo[buf].buftype == "terminal"
+            end,
+          },
+          "Trouble",
+          { ft = "qf", title = "QuickFix" },
+          {
+            ft = "help",
+            size = { height = 20 },
+            -- only show help buffers
+            filter = function(buf)
+              return vim.bo[buf].buftype == "help"
+            end,
+          },
+        },
+        left = {
+          {
+            title = "Neo-Tree",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "filesystem"
+            end,
+            pinned = true,
+            size = { height = 0.4 },
+          },
+          {
+            title = "Neo-Tree Buffers",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "buffers"
+            end,
+            pinned = true,
+            open = "Neotree position=top buffers",
+          },
+          {
+            title = "Neo-Tree Git",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "git_status"
+            end,
+            pinned = true,
+            open = "Neotree position=right git_status",
+          },
+          {
+            ft = "Outline",
+            pinned = true,
+            open = "SymbolsOutlineOpen",
+          },
+          -- any other neo-tree windows
+          "neo-tree",
+        },
+      },
+    },
 }
