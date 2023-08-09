1f621c8e69a001fb14d2aa2a1f8cede7df511d0d
Simon Mutch <smutch@unimelb.edu.au>
2023-08-09 13:53:43 +1000

    Automatically identify hatch projects

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -30,6 +30,16 @@ M.file_exists = function(name)
 end
 
 
+local function is_hatch_project()
+    if Path:new("./pyproject.toml"):exists() then
+        local head = Path:new("./pyproject.toml"):head(10)
+        if string.find(head, "hatchling") then
+            return true
+        end
+    end
+    return false
+end
+
 local python_prefix = "/usr"
 if vim.env.VIRTUAL_ENV then
     python_prefix = vim.env.VIRTUAL_ENV
@@ -37,6 +47,8 @@ elseif Path:new("./poetry.lock"):exists() then
     python_prefix = string.sub(vim.fn.system('poetry env info --path'), 0, -2)
 elseif vim.env.CONDA_PREFIX then
     python_prefix = vim.env.CONDA_PREFIX
+elseif is_hatch_project() then
+    python_prefix = string.sub(vim.fn.system('hatch env find'), 0, -2)
 elseif Path:new(vim.env.HOME .. "/.pyenv/shims/python"):exists() then
     python_prefix = "/Users/smutch/.pyenv/shims/python"
 end
