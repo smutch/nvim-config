2a464fc2e82fa1fff44f52c61ebfce3789217df2
Simon Mutch <smutch@unimelb.edu.au>
2023-11-09 10:33:32 +1100

    Make tabline more intuitive

--- lua/plugins/aesthetics.lua
+++ lua/plugins/aesthetics.lua
@@ -143,6 +143,21 @@ return {
                         { ' 󰯉 ', hl = theme.head },
                         line.sep('', theme.head, theme.fill),
                     },
+                    line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+                        if win.buf().type() == 'nofile' then
+                            return ''
+                        end
+                        return {
+                            line.sep('', theme.win, theme.fill),
+                            win.is_current() and '' or '',
+                            win.buf_name(),
+                            win.buf().is_changed() and { '', hl = {fg = theme.head.bg }} or '',
+                            line.sep('', theme.win, theme.fill),
+                            hl = theme.win,
+                            margin = ' ',
+                        }
+                    end),
+                    line.spacer(),
                     line.tabs().foreach(function(tab)
                         local hl = tab.is_current() and theme.current_tab or theme.tab
                         return {
@@ -153,20 +168,6 @@ return {
                             tab.close_btn(''),
                             line.sep('', hl, theme.fill),
                             hl = hl,
-                            margin = ' ',
-                        }
-                    end),
-                    line.spacer(),
-                    line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-                        if win.buf().type() == 'nofile' then
-                            return ''
-                        end
-                        return {
-                            line.sep('', theme.win, theme.fill),
-                            win.is_current() and '' or '',
-                            win.buf_name(),
-                            line.sep('', theme.win, theme.fill),
-                            hl = theme.win,
                             margin = ' ',
                         }
                     end),
