64db4bc66859a80ad7652524de72f4fc6a3f82ee
Simon Mutch <smutch@unimelb.edu.au>
2024-04-04 20:54:56 +1100

    Replace IBL with hlchunk

--- lua/plugins/aesthetics.lua
+++ lua/plugins/aesthetics.lua
@@ -43,13 +43,29 @@ return {
         config = require 'plugins.config.lualine'.config,
     },
     {
-        'lukas-reineke/indent-blankline.nvim',
+        "shellRaining/hlchunk.nvim",
+        event = { "UIEnter" },
         config = function()
-            require("ibl").setup {
-                indent = { char = '│' },
-                scope = { enabled = true, show_start = false, show_end = false, highlight = { "Number" } },
-                exclude = { buftypes = { "terminal" } }
-            }
+            require("hlchunk").setup({
+                chunk = {
+                    enable = true,
+                    use_treesitter = true,
+                    chars = {
+                        horizontal_line = "━",
+                        vertical_line = "┃",
+                        left_top = "┏",
+                        left_bottom = "┗",
+                        right_arrow = "━",
+                    },
+                },
+                blank = {
+                    enable = false,
+                },
+                line_num = {
+                    enable = false,
+                    use_treesitter = true,
+                },
+            })
         end
     },
     {
