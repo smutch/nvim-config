2002e99d572b12011e14a8637b6ed0753b456d81
Simon Mutch <smutch@unimelb.edu.au>
2024-04-08 12:21:40 +1000

    utils: add harpoon

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -390,4 +390,74 @@ return {
             vim.keymap.set('n', '<leader>oo', '<CMD>OverseerQuickAction restart<CR>', { noremap = true })
         end,
     },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+        config = function()
+            local harpoon = require "harpoon"
+            harpoon:setup {}
+
+            local conf = require("telescope.config").values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require("telescope.pickers").new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                }):find()
+            end
+
+            vim.keymap.set("n", "<leader>hq", function()
+                toggle_telescope(harpoon:list())
+            end, { noremap = true, desc = "Harpoon - Toggle Quick Menu" })
+            vim.keymap.set("n", "<leader>ha", function()
+                harpoon:list():add()
+            end, { noremap = true, desc = "Harpoon - Add" })
+            vim.keymap.set("n", "<leader>hl", function()
+                harpoon:list():next()
+            end, { noremap = true, desc = "Harpoon - Next" })
+            vim.keymap.set("n", "<leader>hh", function()
+                harpoon:list():prev()
+            end, { noremap = true, desc = "Harpoon - Prev" })
+            for ii = 1, 9 do
+                vim.keymap.set("n", "<leader>h" .. ii, function()
+                    require "harpoon":list():select(ii)
+                end, { noremap = "true", desc = "Harpoon - Select " .. ii }
+                )
+            end
+        end
+        -- keys = vim.list_extend({
+        --     -- {
+        --     --     "<leader>hq",
+        --     --     function()
+        --     --         local harpoon = require "harpoon"
+        --     --         harpoon.ui:toggle_quick_menu(harpoon:list())
+        --     --     end,
+        --     --     desc = "Harpoon - Toggle Quick Menu"
+        --     -- },
+        --     {
+        --         "<leader>ha",
+        --         function() require "harpoon":list():add() end,
+        --         desc = "Harpoon - Add"
+        --     },
+        --     {
+        --         "<leader>hl",
+        --         function() require "harpoon":list():next() end,
+        --         desc = "Harpoon - Next"
+        --     },
+        --     {
+        --         "<leader>hh",
+        --         function() require "harpoon":list():prev() end,
+        --         desc = "Harpoon - Previous"
+        --     },
+        -- }, harpoon_maps),
+    },
 }
