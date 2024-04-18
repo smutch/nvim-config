a4eff1aabb382e6f78b3b9d94d68753e257b20fd
Simon Mutch <smutch@unimelb.edu.au>
2024-04-18 11:52:36 +1000

    make all treesitter movement mappings uppercase

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -26,10 +26,10 @@ return {
 
                         keymaps = {
                             -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
+                            ["aF"] = "@function.outer",
+                            ["iF"] = "@function.inner",
+                            ["aC"] = "@class.outer",
+                            ["iC"] = "@class.inner",
                             ["aP"] = "@parameter.outer",
                             ["iP"] = "@parameter.inner",
                             ["aC"] = "@comment.outer",
@@ -57,21 +57,21 @@ return {
                     },
                     move = {
                         enable = true,
-                        set_jumps = false,     -- whether to set jumps in the jumplist
+                        set_jumps = false, -- whether to set jumps in the jumplist
                         goto_next_start = {
-                            ["]m"] = "@function.outer",
+                            ["]f"] = "@function.outer",
                             ["]]"] = "@class.outer",
                         },
                         goto_next_end = {
-                            ["]M"] = "@function.outer",
+                            ["]F"] = "@function.outer",
                             ["]["] = "@class.outer",
                         },
                         goto_previous_start = {
-                            ["[m"] = "@function.outer",
+                            ["[f"] = "@function.outer",
                             ["[["] = "@class.outer",
                         },
                         goto_previous_end = {
-                            ["[M"] = "@function.outer",
+                            ["[F"] = "@function.outer",
                             ["[]"] = "@class.outer",
                         },
                     },
