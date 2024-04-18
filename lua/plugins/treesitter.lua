d8d35c48d7f38fc1c1640edf9faedd7a42936de3
Simon Mutch <smutch@unimelb.edu.au>
2024-04-18 12:33:37 +1000

    treesitter: ensure diff parser installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
                     custom_captures = { ["variable"] = "Normal" },
                     additional_vim_regex_highlighting = false -- WARNING: Must be set to false for spellsitter plugin
                 },
-                ensure_installed = { "c", "python", "lua", "bash", "make", "cmake", "toml", "yaml", "cpp", "cuda", "json", "rust", "vim", "html", "css", "bibtex", "markdown", "rst", "comment" },
+                ensure_installed = { "c", "python", "lua", "bash", "make", "cmake", "toml", "yaml", "cpp", "cuda", "json", "rust", "vim", "html", "css", "bibtex", "markdown", "rst", "comment", "diff" },
                 incremental_selection = { enable = false },
                 refactor = {
                     highlight_definitions = { enable = true },
