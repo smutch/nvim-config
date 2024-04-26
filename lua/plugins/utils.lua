0b9bb9b6fd5d9b37b1cf919c34e9025422484eac
Simon Mutch <smutch@unimelb.edu.au>
2024-04-26 11:31:12 +1000

    Add floating nvim-tree with g-

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -435,4 +435,56 @@ return {
             end
         end
     },
+    {
+        'nvim-tree/nvim-tree.lua',
+        keys = {
+            {"g-", "<CMD>NvimTreeToggle<CR>", desc = "NvimTree - Toggle"},
+        },
+        config = function()
+            local HEIGHT_RATIO = 0.8
+            local WIDTH_RATIO = 0.5
+
+            require('nvim-tree').setup({
+                view = {
+                    float = {
+                        enable = true,
+                        open_win_config = function()
+                            local screen_w = vim.opt.columns:get()
+                            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                            local window_w = screen_w * WIDTH_RATIO
+                            local window_h = screen_h * HEIGHT_RATIO
+                            local window_w_int = math.floor(window_w)
+                            local window_h_int = math.floor(window_h)
+                            local center_x = (screen_w - window_w) / 2
+                            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                                - vim.opt.cmdheight:get()
+                            return {
+                                border = 'rounded',
+                                relative = 'editor',
+                                row = center_y,
+                                col = center_x,
+                                width = window_w_int,
+                                height = window_h_int,
+                            }
+                        end,
+                    },
+                    width = function()
+                        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+                    end,
+                },
+            })
+
+            vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+                pattern = 'NvimTree*',
+                callback = function()
+                    local api = require('nvim-tree.api')
+                    local view = require('nvim-tree.view')
+
+                    if not view.is_visible() then
+                        api.tree.open()
+                    end
+                end,
+            })
+        end,
+    }
 }
