f7bd31838b31288f6991ddef8fba48c465446c7d
Simon Mutch <smutch@unimelb.edu.au>
2024-06-21 16:01:32 +1000

    Update harpoon telescope config to work with most recent versions

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -289,31 +289,52 @@ return {
 
             local conf = require("telescope.config").values
             local function toggle_telescope(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
+                local finder = function()
+                    local paths = {}
+                    for _, item in ipairs(harpoon_files.items) do
+                        table.insert(paths, item.value)
+                    end
+
+                    return require("telescope.finders").new_table({
+                        results = paths,
+                    })
                 end
 
                 require("telescope.pickers").new({}, {
                     prompt_title = "Harpoon",
-                    finder = require("telescope.finders").new_table({
-                        results = file_paths,
-                    }),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
+                    finder = finder(),
+                    previewer = false,
+                    sorter = require("telescope.config").values.generic_sorter({}),
+                    layout_config = {
+                        height = 0.4,
+                        width = 0.5,
+                        prompt_position = "top",
+                        preview_cutoff = 120,
+                    },
+                    attach_mappings = function(prompt_bufnr, map)
+                        map("i", "<C-d>", function()
+                            local state = require("telescope.actions.state")
+                            local selected_entry = state.get_selected_entry()
+                            local current_picker = state.get_current_picker(prompt_bufnr)
+
+                            table.remove(harpoon_files.items, selected_entry.index)
+                            current_picker:refresh(finder())
+                        end)
+                        return true
+                    end,
                 }):find()
             end
 
-            vim.keymap.set("n", "<leader>hq", function()
+            vim.keymap.set("n", "<leader>hl", function()
                 toggle_telescope(harpoon:list())
             end, { noremap = true, desc = "Harpoon - Toggle Quick Menu" })
             vim.keymap.set("n", "<leader>ha", function()
                 harpoon:list():add()
             end, { noremap = true, desc = "Harpoon - Add" })
-            vim.keymap.set("n", "<leader>hl", function()
+            vim.keymap.set("n", "<leader>hn", function()
                 harpoon:list():next()
             end, { noremap = true, desc = "Harpoon - Next" })
-            vim.keymap.set("n", "<leader>hh", function()
+            vim.keymap.set("n", "<leader>hp", function()
                 harpoon:list():prev()
             end, { noremap = true, desc = "Harpoon - Prev" })
             for ii = 1, 9 do
