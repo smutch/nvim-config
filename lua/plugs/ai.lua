9bac58a9e21be6fded32fdca22d56adb693683e2
Simon Mutch <smutch@unimelb.edu.au>
2024-10-29 10:50:12 +1100

    Try codecompanion instead of avanti

--- lua/plugs/ai.lua
+++ lua/plugs/ai.lua
@@ -18,23 +18,54 @@ return {
             })
         end
     },
+    -- {
+    --     "yetone/avante.nvim",
+    --     event = "VeryLazy",
+    --     cmd = "AvanteAsk",
+    --     build = "make",
+    --     opts = {
+    --         provider = (function()
+    --             local provider = require "system".avante_provider
+    --             if provider == nil then provider = "copilot" end
+    --             return provider
+    --         end)(),
+    --     },
+    --     dependencies = {
+    --         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    --         "stevearc/dressing.nvim",
+    --         "nvim-lua/plenary.nvim",
+    --         "MunifTanjim/nui.nvim",
+    --     },
+    -- },
     {
-        "yetone/avante.nvim",
+        "olimorris/codecompanion.nvim",
         event = "VeryLazy",
-        cmd = "AvanteAsk",
-        build = "make",
-        opts = {
-            provider = (function()
-                local provider = require "system".avante_provider
-                if provider == nil then provider = "copilot" end
-                return provider
-            end)(),
-        },
         dependencies = {
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp",                      -- Optional: For using slash commands and variables in the chat buffer
+            "nvim-telescope/telescope.nvim",         -- Optional: For using slash commands
+            { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
         },
-    },
+        config = function()
+            require "codecompanion".setup {
+                strategies = {
+                    chat = {
+                        adapter = "copilot",
+                    },
+                    inline = {
+                        adapter = "copilot",
+                    },
+                },
+            }
+            vim.cmd([[cab cc CodeCompanion]])
+        end,
+        keys = {
+            { "<M-a>",          "<cmd>CodeCompanionActions<cr>",     mode = "n" },
+            { "<M-a>",          "<cmd>CodeCompanionActions<cr>",     mode = "v" },
+            { "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = "n" },
+            { "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = "v" },
+            { "ga",             "<cmd>CodeCompanionChat Add<cr>",    mode = "v" },
+        }
+    }
 }
