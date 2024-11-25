aed4e4805950c067a1915009001b51e483769e8f
Simon Mutch <smutch@unimelb.edu.au>
2024-11-26 09:25:18 +1100

    Add obsidian plugin

--- lua/plugs/utils.lua
+++ lua/plugs/utils.lua
@@ -233,7 +233,7 @@ return {
             { '<leader>R',  function() require('smart-splits').start_resize_mode() end, desc = "smart-splits - enter resize mode" },
         }
     },
-    { 'tiagovla/scope.nvim', config = true },
+    { 'tiagovla/scope.nvim',        config = true },
     {
         'shortcuts/no-neck-pain.nvim',
         config = function()
@@ -453,5 +453,46 @@ return {
             { "<leader>nh", function() Snacks.notifier.show_history() end, desc = "(N)otify (h)istory" },
             { "<leader>nc", function() Snacks.notifier.hide() end,         desc = "(N)otify (c)lear" },
         },
-    }
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+        -- event = {
+        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+        --   -- refer to `:h file-pattern` for more examples
+        --   "BufReadPre path/to/my-vault/*.md",
+        --   "BufNewFile path/to/my-vault/*.md",
+        -- },
+        dependencies = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
+
+            -- see below for full list of optional dependencies 👇
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "work",
+                    path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/work",
+                },
+            },
+            daily_notes = {
+                folder = "dailies",
+                date_format = "%Y-%m-%d",
+                template = "daily.md"
+            },
+            templates = {
+                folder = "_templates",
+                date_format = "%Y-%m-%d",
+                time_format = "%H:%M",
+                substitutions = {},
+            },
+
+            -- see below for full list of options 👇
+        },
+    },
 }
