558ebafbbf705260e79ec1cf154ad7a489383575
Simon Mutch <smutch@unimelb.edu.au>
2024-11-28 14:57:02 +1100

    Add jupytext.nvim

--- lua/plugs/filetypes.lua
+++ lua/plugs/filetypes.lua
@@ -19,19 +19,30 @@ return {
         ft = { 'markdown', 'pandoc', 'quarto', 'markdown.pandoc', 'Avante', 'codecompanion' },
         opts = {},
     },
-    -- {
-    --     'MeanderingProgrammer/markdown.nvim',
-    --     name = 'render-markdown',
-    --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    --     opts = { file_types = { 'markdown.pandoc', 'markdown', 'quarto', 'pandoc' }, },
-    --     ft = { 'markdown', 'pandoc', 'quarto', 'markdown.pandoc' },
-    -- },
     {
         'snakemake/snakemake',
         ft = 'snakemake',
         config = function(plugin)
             vim.opt.rtp:append(plugin.dir .. "/misc/vim")
         end
+    },
+    { -- directly open ipynb files as quarto docuements
+        -- and convert back behind the scenes
+        'GCBallesteros/jupytext.nvim',
+        opts = {
+            custom_language_formatting = {
+                python = {
+                    extension = 'qmd',
+                    style = 'quarto',
+                    force_ft = 'quarto',
+                },
+                r = {
+                    extension = 'qmd',
+                    style = 'quarto',
+                    force_ft = 'quarto',
+                },
+            },
+        },
     },
     {
         'quarto-dev/quarto-nvim',
@@ -51,10 +62,25 @@ return {
                     },
                     completion = {
                         enabled = true
-                    }
+                    },
+                    codeRunner = {
+                        enabled = true,
+                        default_method = 'molten',
+                        never_run = { "yaml" }, -- filetypes which are never sent to a code runner
+                    },
                 }
             }
-        end
+            local runner = require("quarto.runner")
+            vim.keymap.set("n", "gz", runner.run_cell, { desc = "run cell", silent = true })
+            vim.keymap.set("n", "<localleader>qa", runner.run_above, { desc = "run cell and above", silent = true })
+            vim.keymap.set("n", "<localleader>qA", runner.run_all, { desc = "run all cells", silent = true })
+            vim.keymap.set("n", "<localleader>ql", runner.run_line, { desc = "run line", silent = true })
+            vim.keymap.set("v", "gz", runner.run_range, { desc = "run visual range", silent = true })
+            vim.keymap.set("n", "<localleader>QA", function()
+                runner.run_all(true)
+            end, { desc = "run all cells of all languages", silent = true })
+        end,
+        cmd = { "QuartoPreview", "QuartoActivate" }
     },
     {
         'kaarmu/typst.vim',
