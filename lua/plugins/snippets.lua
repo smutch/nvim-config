50c3f07e593d9fa2feec07ee15b7845d363314bf
Simon Mutch <smutch@unimelb.edu.au>
2025-05-19 14:59:45 +1000

    Use <m-j> to also expand snippet if possible

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -3,28 +3,27 @@ return {
         'L3MON4D3/LuaSnip',
         dependencies = { 'rafamadriz/friendly-snippets' },
         config = function()
+            local ls = require("luasnip")
+
             function _G.luasnip_map_forward()
-                local luasnip = require 'luasnip'
-                if luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
+                if ls.expand_or_jumpable() then
+                    ls.expand_or_jump()
                     return true
                 end
                 return false
             end
 
             function _G.luasnip_map_backward()
-                local luasnip = require 'luasnip'
-                if luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
+                if ls.jumpable(-1) then
+                    ls.jump(-1)
                     return true
                 end
                 return false
             end
 
-            vim.keymap.set('i', '<A-j>', '<cmd>call v:lua.luasnip_map_forward()<cr>')
-            vim.keymap.set('s', '<A-j>', '<cmd>call v:lua.luasnip_map_forward()<cr>')
-            vim.keymap.set('i', '<A-k>', '<cmd>call v:lua.luasnip_map_backward()<cr>')
-            vim.keymap.set('s', '<A-k>', '<cmd>call v:lua.luasnip_map_backward()<cr>')
+            vim.keymap.set({'i', 's'}, '<A-j>', function() ls.expand_or_jump() end, {silent = true})
+            vim.keymap.set({'i', 's'}, '<A-k>', function() ls.jump(-1) end, {silent = true})
+
             vim.api.nvim_create_user_command("LuaSnipEdit", require("luasnip.loaders").edit_snippet_files, {})
 
             require("luasnip.loaders.from_vscode").lazy_load()
