7ce9b27ffc2ff9748e6711250b42204c1c649aed
Simon Mutch <smutch@unimelb.edu.au>
2025-05-20 11:59:10 +1000

    Try out arrow over harpoon

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -298,72 +298,15 @@ return {
         },
     },
     {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        event = "VeryLazy",
-        config = function()
-            local harpoon = require("harpoon")
-            harpoon:setup({})
-
-            -- local conf = require("telescope.config").values
-            -- local function toggle_telescope(harpoon_files)
-            --     local finder = function()
-            --         local paths = {}
-            --         for _, item in ipairs(harpoon_files.items) do
-            --             table.insert(paths, item.value)
-            --         end
-
-            --         return require("telescope.finders").new_table({
-            --             results = paths,
-            --         })
-            --     end
-
-            --     require("telescope.pickers")
-            --         .new({}, {
-            --             prompt_title = "Harpoon",
-            --             finder = finder(),
-            --             previewer = false,
-            --             sorter = require("telescope.config").values.generic_sorter({}),
-            --             layout_config = {
-            --                 height = 0.4,
-            --                 width = 0.5,
-            --                 prompt_position = "top",
-            --                 preview_cutoff = 120,
-            --             },
-            --             attach_mappings = function(prompt_bufnr, map)
-            --                 map("i", "<C-d>", function()
-            --                     local state = require("telescope.actions.state")
-            --                     local selected_entry = state.get_selected_entry()
-            --                     local current_picker = state.get_current_picker(prompt_bufnr)
-
-            --                     table.remove(harpoon_files.items, selected_entry.index)
-            --                     current_picker:refresh(finder())
-            --                 end)
-            --                 return true
-            --             end,
-            --         })
-            --         :find()
-            -- end
-
-            -- vim.keymap.set("n", "<leader>hl", function()
-            --     toggle_telescope(harpoon:list())
-            -- end, { noremap = true, desc = "Harpoon - Toggle Quick Menu" })
-            vim.keymap.set("n", "<leader>ha", function()
-                harpoon:list():add()
-            end, { noremap = true, desc = "Harpoon - Add" })
-            vim.keymap.set("n", "<leader>hn", function()
-                harpoon:list():next()
-            end, { noremap = true, desc = "Harpoon - Next" })
-            vim.keymap.set("n", "<leader>hp", function()
-                harpoon:list():prev()
-            end, { noremap = true, desc = "Harpoon - Prev" })
-            for ii = 1, 9 do
-                vim.keymap.set("n", "<leader>h" .. ii, function()
-                    require("harpoon"):list():select(ii)
-                end, { noremap = true, desc = "Harpoon - Select " .. ii })
-            end
-        end,
+        "otavioschwanck/arrow.nvim",
+        dependencies = {
+            { "echasnovski/mini.icons" },
+        },
+        opts = {
+            show_icons = true,
+            leader_key = "M", -- Recommended to be a single key
+            buffer_leader_key = "m", -- Per Buffer Mappings
+        },
     },
     {
         "nvim-tree/nvim-tree.lua",
