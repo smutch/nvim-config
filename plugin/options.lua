d2369c984b7f8e7a09997ff1fed3a9d1acd78b04
Simon Mutch <smutch@unimelb.edu.au>
2025-05-23 10:51:49 +1000

    Allow sourcing of project specific config files using env vars

--- plugin/options.lua
+++ plugin/options.lua
@@ -6,15 +6,26 @@ vim.o.linebreak = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.laststatus = 3
-vim.opt.listchars = { tab = [[▸\ ]], eol = '↵', trail = '·' }
+vim.opt.listchars = { tab = [[▸\ ]], eol = "↵", trail = "·" }
 vim.o.number = false -- Don't show line numbers
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.opt.splitkeep = "screen"
 
-vim.o.backupdir = os.getenv('HOME') .. "/.nvim_backup"
-vim.o.directory = os.getenv('HOME') .. "/.nvim_backup"
-vim.o.undodir = os.getenv('HOME') .. "/.nvim_backup/undo"
+vim.o.backupdir = os.getenv("HOME") .. "/.nvim_backup"
+vim.o.directory = os.getenv("HOME") .. "/.nvim_backup"
+vim.o.undodir = os.getenv("HOME") .. "/.nvim_backup/undo"
 vim.o.undofile = true
 
-vim.opt.diffopt:append { "algorithm:patience" }
+vim.opt.diffopt:append({ "algorithm:patience" })
+
+-- Include project specific options
+if vim.env.EXTRA_NVIM then
+    local paths = vim.split(vim.env.EXTRA_NVIM, ":", { plain = true, trimempty = true })
+    for _, path in ipairs(paths) do
+        local success, err = pcall(dofile, path)
+        if not success then
+            vim.notify("Error loading " .. path .. ": " .. tostring(err), vim.log.levels.ERROR)
+        end
+    end
+end
