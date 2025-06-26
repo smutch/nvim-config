d1625a96e24feab6f3d519aa4c2955134ce62a76
Simon Mutch <smutch@unimelb.edu.au>
2025-06-27 08:50:25 +1000

    Show all files with file picker

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -535,7 +535,7 @@ return {
             {
                 "<leader>ff",
                 function()
-                    Snacks.picker.files()
+                    Snacks.picker.files({hidden = true, ignored = true})
                 end,
                 desc = "Find Files",
             },
