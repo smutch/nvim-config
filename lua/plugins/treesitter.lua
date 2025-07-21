489edd1f64817881b687c67e2afb24cbe2fb0252
Simon Mutch <smutch@unimelb.edu.au>
2025-07-21 10:04:55 +1000

    Ensure markview loaded before treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        requires = { "OXY2DEV/markview.nvim" },
         config = function() require 'plugins.config.treesitter' end
     },
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
