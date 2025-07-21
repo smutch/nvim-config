a3f6d90a47af2356d3940103bd3cfb9ebcfe4398
Simon Mutch <smutch@unimelb.edu.au>
2025-07-21 10:04:37 +1000

    Remove copilot-lsp
    I never use the next edit completions. Barely ever works or gives
    anything useful.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -25,55 +25,6 @@ return {
         end,
     },
     {
-        "copilotlsp-nvim/copilot-lsp",
-        enabled = function()
-            local rc, system = pcall(require, "system")
-            if rc then
-                return system.enable_copilot or false and true
-            else
-                return false
-            end
-        end,
-        init = function()
-            vim.g.copilot_nes_debounce = 500
-            -- c.f. https://github.com/copilotlsp-nvim/copilot-lsp/issues/8#issuecomment-2822027212
-            vim.lsp.config("copilot_ls", {
-                on_init = function(client)
-                    vim.api.nvim_set_hl(0, "NesAdd", { link = "DiffAdd", default = true })
-                    vim.api.nvim_set_hl(0, "NesDelete", { link = "DiffDelete", default = true })
-                    vim.api.nvim_set_hl(0, "NesApply", { link = "DiffText", default = true })
-
-                    local au = vim.api.nvim_create_augroup("copilot-language-server", { clear = true })
-
-                    --NOTE: didFocus
-                    vim.api.nvim_create_autocmd("BufEnter", {
-                        callback = function()
-                            local td_params = vim.lsp.util.make_text_document_params()
-                            client:notify("textDocument/didFocus", {
-                                textDocument = {
-                                    uri = td_params.uri,
-                                },
-                            })
-                        end,
-                        group = au,
-                    })
-
-                    vim.keymap.set("n", "<tab>", function()
-                        local state = vim.b[vim.api.nvim_get_current_buf()].nes_state
-                        if not state then
-                            require("copilot-lsp.nes").request_nes(client)
-                            vim.notify("Requesting Copilot LSP suggestions...")
-                        else
-                            local _ = require("copilot-lsp.nes").apply_pending_nes()
-                                and require("copilot-lsp.nes").walk_cursor_end_edit()
-                        end
-                    end)
-                end,
-            })
-            vim.lsp.enable("copilot_ls")
-        end,
-    },
-    {
         "olimorris/codecompanion.nvim",
         event = "VeryLazy",
         dependencies = {
