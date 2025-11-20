912b76681a1a9c2c427157769a72ae1fdd153fd5
Simon Mutch <smutch@unimelb.edu.au>
2025-11-20 13:01:23 +1100

    Set the environemnt automatically for uv scripts

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -1,8 +1,16 @@
 local h = require("helpers")
 
+local interpreter_path = h.python_interpreter_path
+
+if vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]:match("^ *# /// script") then
+    print("Bufname: " .. vim.api.nvim_buf_get_name(0))
+    interpreter_path =
+        vim.system({ "uv", "python", "find", "--script", vim.api.nvim_buf_get_name(0) }):wait().stdout:gsub("\n", "")
+end
+
 return {
     settings = {
         basedpyright = { analysis = { typeCheckingMode = "standard", disableOrganizeImports = true } },
-        python = { pythonPath = h.python_interpreter_path },
+        python = { pythonPath = interpreter_path },
     },
 }
--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,12 +1,16 @@
-local Path = require 'plenary.path'
+local Path = require("plenary.path")
 
 M = {}
 
 M.file_exists = function(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
 end
-
 
 local function is_hatch_project()
     if Path:new("./pyproject.toml"):exists() then
@@ -24,13 +28,13 @@ if vim.g.python_prefix then
 elseif vim.env.VIRTUAL_ENV then
     python_prefix = vim.env.VIRTUAL_ENV
 elseif Path:new("./poetry.lock"):exists() then
-    python_prefix = string.sub(vim.fn.system('poetry env info --path'), 0, -2)
+    python_prefix = string.sub(vim.fn.system("poetry env info --path"), 0, -2)
 elseif Path:new("./pixi.lock"):exists() then
     python_prefix = string.sub(vim.fn.system('pixi --quiet run --locked "which python"'), 0, -13)
 elseif vim.env.CONDA_PREFIX then
     python_prefix = vim.env.CONDA_PREFIX
 elseif is_hatch_project() then
-    python_prefix = string.sub(vim.fn.system('hatch env find'), 0, -2)
+    python_prefix = string.sub(vim.fn.system("hatch env find"), 0, -2)
 elseif Path:new(vim.env.HOME .. "/.pyenv/shims/python"):exists() then
     python_prefix = "/Users/smutch/.pyenv/shims/python"
 end
@@ -43,5 +47,26 @@ end
 M.python_interpreter_path = python_interpreter_path
 M.python_prefix = python_prefix
 vim.g.python_prefix = python_prefix
+
+function M.fixssh()
+    local h = io.popen("tmux show-environment")
+    if not h then
+        return
+    end
+    for line in h:lines() do
+        local k, v = line:match("^([^=]+)=(.*)$")
+        if k and k:match("^SSH_") then
+            -- remove surrounding quotes if tmux quoted them
+            v = v:gsub('^"(.*)"$', "%1")
+            vim.env[k] = v
+        end
+    end
+    h:close()
+    vim.notify("SSH environment imported from tmux", vim.log.levels.INFO)
+end
+
+vim.api.nvim_create_user_command("FixSSH", function()
+    M.fixssh()
+end, {})
 
 return M
