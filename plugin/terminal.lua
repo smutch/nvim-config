f7af42f888d440f783710aca9950dab22f2d54fa
Simon Mutch <smutch@unimelb.edu.au>
2026-01-05 12:29:10 +1100

    Formatting

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -7,8 +7,8 @@ vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
     group = term_augroup,
     callback = function()
         local status, Color = pcall(require, "nightfox.lib.color")
-        if (status) then
-            local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+        if status then
+            local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
             local bg = Color.from_hex(string.format("%06x", normal.bg)):brighten(-3):to_hex()
             vim.api.nvim_set_hl(0, "TermNormal", { bg = bg })
         end
@@ -17,14 +17,14 @@ vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
         vim.wo.number = false
         vim.wo.relativenumber = false
         -- vim.cmd.startinsert()
-    end
+    end,
 })
 
-vim.keymap.set('t', 'kj', [[<C-\><C-n>]])
-vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]])
-vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]])
-vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]])
-vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]])
-vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
-vim.keymap.set('n', '<leader>ts', ':botright 20split | setl wfh | term<cr>')
-vim.keymap.set('n', '<leader>tv', ':botright 80vsplit | setl wfh | term<cr>')
+vim.keymap.set("t", "kj", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]])
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]])
+vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]])
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]])
+vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
+vim.keymap.set("n", "<leader>ts", ":botright 20split | setl wfh | term<cr>")
+vim.keymap.set("n", "<leader>tv", ":botright 80vsplit | setl wfh | term<cr>")
