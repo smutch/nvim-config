a148930c27c6a455668b2af14aeab0d171d82f20
Simon Mutch <smutch@unimelb.edu.au>
2026-04-17 11:44:36 +1000

    treesitter using arborist

---
+++ plugin/treesitter.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+    "https://github.com/arborist-ts/arborist.nvim",
+    { src = "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+    "nvim-treesitter/playground",
+})
