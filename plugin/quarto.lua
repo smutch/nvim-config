9b5605d33cc41ee17f56d0d61084ee79053d21ad
Simon Mutch <smutch@unimelb.edu.au>
2026-04-27 17:13:39 +1000

    fix broken quarto mappings

--- plugin/quarto.lua
+++ plugin/quarto.lua
@@ -40,25 +40,15 @@ require("load").on_event("FileType~quarto", function()
         },
     })
     local runner = require("quarto.runner")
-    vim.keymap.set("n", "gz", runner.run_cell, { desc = "run cell", silent = true, nnoremap = true })
-    vim.keymap.set(
-        "n",
-        "<localleader>qa",
-        runner.run_above,
-        { desc = "run cell and above", silent = true, nnoremap = true }
-    )
-    vim.keymap.set("n", "<localleader>qA", runner.run_all, { desc = "run all cells", silent = true, nnoremap = true })
-    vim.keymap.set("n", "<localleader>ql", runner.run_line, { desc = "run line", silent = true, nnoremap = true })
-    vim.keymap.set("v", "gz", runner.run_range, { desc = "run visual range", silent = true, nnoremap = true })
-    vim.keymap.set(
-        "n",
-        "<localleader>qp",
-        require("quarto").quartoPreview,
-        { desc = "quarto preview", silent = true, nnoremap = true }
-    )
+    vim.keymap.set("n", "gz", runner.run_cell, { desc = "run cell", silent = true })
+    vim.keymap.set("n", "<localleader>qa", runner.run_above, { desc = "run cell and above", silent = true })
+    vim.keymap.set("n", "<localleader>qA", runner.run_all, { desc = "run all cells", silent = true })
+    vim.keymap.set("n", "<localleader>ql", runner.run_line, { desc = "run line", silent = true })
+    vim.keymap.set("v", "gz", runner.run_range, { desc = "run visual range", silent = true })
+    vim.keymap.set("n", "<localleader>qp", require("quarto").quartoPreview, { desc = "quarto preview", silent = true })
     vim.keymap.set("n", "<localleader>QA", function()
         runner.run_all(true)
-    end, { desc = "run all cells of all languages", silent = true, nnoremap = true })
+    end, { desc = "run all cells of all languages", silent = true })
 
     vim.filetype.add({
         extension = {
