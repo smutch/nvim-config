6d555f2d6cbcf281ecf3dc6a9f2ca9d15726bb03
Simon Mutch <smutch@unimelb.edu.au>
2026-09-01 14:45:45 +1000

    add a typst tempalte to overseer config

--- plugin/overseer.lua
+++ plugin/overseer.lua
@@ -1,6 +1,7 @@
 require("load").later(function()
     vim.pack.add(require("load").gh({ "stevearc/overseer.nvim" }))
-    require("overseer").setup({
+    local overseer = require("overseer")
+    overseer.setup({
         task_list = {
             bindings = {
                 ["<C-l>"] = false,
@@ -19,7 +20,30 @@ require("load").later(function()
     vim.keymap.set("n", "<leader>ot", "<CMD>OverseerToggle<CR>", { desc = "Overseer - toggle" })
     vim.keymap.set("n", "<leader>or", "<CMD>OverseerRun<CR>", { desc = "Overseer - run" })
     vim.keymap.set("n", "<leader>oo", function()
-        require("overseer").list_tasks()[1]:restart(true)
+        overseer.list_tasks()[1]:restart(true)
     end, { desc = "Overseer - restart" })
-    vim.keymap.set("n", "<leader>ob", "<CMD>OverseerLoadBundle<CR>", { desc = "Overseer - load bundle" })
+
+    overseer.register_template({
+        name = "Typst watch",
+        builder = function()
+            return {
+                cmd = { "typst", "watch", vim.fn.expand("%") },
+            }
+        end,
+        condition = {
+            filetype = { "typst" },
+        },
+    })
+    overseer.register_template({
+        name = "Preview with Skim",
+        builder = function()
+            return {
+                -- note we use a string here instead of a table so that we use the shell and can use `open`.
+                cmd = "open -a skim " .. string.gsub(vim.fn.expand("%"), "%.typ$", ".pdf", 1),
+            }
+        end,
+        condition = {
+            filetype = { "typst" },
+        },
+    })
 end)
